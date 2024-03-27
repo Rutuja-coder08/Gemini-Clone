8fc19531b444cc4dc09d6f32b64fdac29964fd86
Rutuja-coder08 <68146768+Rutuja-coder08@users.noreply.github.com>
2024-03-27 12:57:01 +0530

    save prompt in sidebar

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -7,7 +7,7 @@ const ContextProvider = (props) => {
   const [input, setInput] = useState("");
   const [recentPrompt, setRecentPrompt] = useState("");
   const [prevPrompts, setPrevPrompts] = useState([]);
-  const [showResult, setShoeResult] = useState(false);
+  const [showResult, setShowResult] = useState(false);
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState("");
 
@@ -17,14 +17,27 @@ const ContextProvider = (props) => {
     }, 75 * index);
   };
 
+  const newChat = () => {
+    setLoading(false);
+    setShowResult(false);
+  };
   const onSent = async (prompt) => {
     setResultData("");
     setLoading(true);
-    setShoeResult(true);
-    setRecentPrompt(input);
-    const response = await runchat(input);
+    setShowResult(true);
+
+    let response;
+    if (prompt !== undefined) {
+      response = await runchat(prompt);
+      setRecentPrompt(prompt);
+    } else {
+      setPrevPrompts((prev) => [...prev, input]);
+      setRecentPrompt(input);
+      response = await runchat(input);
+    }
+
     let responseArray = response.split("**");
-    let newResponse;
+    let newResponse = "";
     for (let i = 0; i < responseArray.length; i++) {
       if (i === 0 || i % 2 !== 1) {
         newResponse += responseArray[i];
@@ -53,6 +66,7 @@ const ContextProvider = (props) => {
     resultData,
     input,
     setInput,
+    newChat,
   };
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
